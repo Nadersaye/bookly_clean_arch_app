2d0ad856ac3aa37df762979076358624ccebcd76
Nader <ns1568@fayoum.edu.eg>
2024-01-31 19:52:20 +0200

    Add BookModel class

---
+++ lib/Features/home/data/models/book_model.dart
@@ -0,0 +1,33 @@
+import 'package:bookly_clean_arch_app/Features/home/data/models/sale_info_model.dart';
+import 'package:bookly_clean_arch_app/Features/home/data/models/volume_info_model.dart';
+import 'package:bookly_clean_arch_app/Features/home/domain/entities/book_entity.dart';
+
+class BookModel extends BookEntity {
+  final String id;
+  final VolumeInfoModel? volumeInfo;
+  final SaleInfoModel? saleInfo;
+
+  BookModel(
+      {required this.id, required this.volumeInfo, required this.saleInfo})
+      : super(
+            title: volumeInfo?.title ?? 'un known title',
+            author: volumeInfo?.authors?.first ?? 'un known author',
+            image: volumeInfo?.imageLinks?.thumbnail ?? '',
+            bookId: id,
+            price: 0.0,
+            rating: 0,
+            count: 0);
+
+  factory BookModel.fromJson(Map<String, dynamic> json) {
+    return BookModel(
+      id: json['id'],
+      volumeInfo: json['volumeInfo'] == null
+          ? null
+          : VolumeInfoModel.fromJson(
+              json['volumeInfo'] as Map<String, dynamic>),
+      saleInfo: json['saleInfo'] == null
+          ? null
+          : SaleInfoModel.fromJson(json['saleInfo'] as Map<String, dynamic>),
+    );
+  }
+}
