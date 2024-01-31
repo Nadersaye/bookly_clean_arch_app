1ce930dc5922c4d668ea9edf3bde0ba49edad217
Nader <ns1568@fayoum.edu.eg>
2024-01-31 20:06:55 +0200

    Add VolumeInfoModel class with JSON serialization

--- lib/Features/home/data/models/volume_info_model.dart
+++ lib/Features/home/data/models/volume_info_model.dart
@@ -1,1 +1,20 @@
+import 'package:bookly_clean_arch_app/Features/home/data/models/image_links_model.dart';
 
+class VolumeInfoModel {
+  final String? title;
+  final List<String>? authors;
+  final ImageLinksModel? imageLinks;
+
+  VolumeInfoModel(
+      {required this.title, required this.authors, required this.imageLinks});
+
+  factory VolumeInfoModel.fromJson(Map<String, dynamic> json) {
+    return VolumeInfoModel(
+        title: json['title'] as String?,
+        authors: json['authors'] as List<String>?,
+        imageLinks: json['imageLinks'] == null
+            ? null
+            : ImageLinksModel.fromJson(
+                json['imageLinks'] as Map<String, dynamic>));
+  }
+}
