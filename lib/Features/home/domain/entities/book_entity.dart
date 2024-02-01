e581a00012d0f9c6b5dc40ee5a9106d0360d4893
Nader <ns1568@fayoum.edu.eg>
2024-02-01 21:17:03 +0200

    Add Hive annotations and generated code for BookEntity

--- lib/Features/home/domain/entities/book_entity.dart
+++ lib/Features/home/domain/entities/book_entity.dart
@@ -1,12 +1,22 @@
 import 'package:equatable/equatable.dart';
+import 'package:hive/hive.dart';
+part 'book_entity.g.dart';
 
+@HiveType(typeId: 0)
 class BookEntity extends Equatable {
+  @HiveField(0)
   final String bookId;
+  @HiveField(1)
   final String title;
+  @HiveField(2)
   final String author;
+  @HiveField(3)
   final String image;
+  @HiveField(4)
   final num price;
+  @HiveField(5)
   final num rating;
+  @HiveField(6)
   final int count;
 
   const BookEntity({
