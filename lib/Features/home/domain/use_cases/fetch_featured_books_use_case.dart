3d43c1fa12a67e9eeb64f29133eb00f546a330d3
Nader <ns1568@fayoum.edu.eg>
2024-02-02 20:14:13 +0200

    Update import paths in fetch_featured_books_use_case.dart

--- lib/Features/home/domain/use_cases/fetch_featured_books_use_case.dart
+++ lib/Features/home/domain/use_cases/fetch_featured_books_use_case.dart
@@ -1,14 +1,14 @@
 import 'package:bookly_clean_arch_app/Features/home/domain/entities/book_entity.dart';
-import 'package:bookly_clean_arch_app/core/utils/errors/failure.dart';
-import 'package:bookly_clean_arch_app/core/utils/use_cases/base_use_case.dart';
+import 'package:bookly_clean_arch_app/core/errors/failure.dart';
+import 'package:bookly_clean_arch_app/core/use_cases/base_use_case.dart';
 import 'package:dartz/dartz.dart';
 
-import '../repos/base_bookly_repo.dart';
+import '../repos/base_home_repo.dart';
 
 class FetchFeaturedBooksUseCase extends BaseUseCase<List<BookEntity>, int> {
-  final BaseBooklyRepo _baseBooklyRepo;
+  final BaseHomeRepo _baseBooklyRepo;
 
-  FetchFeaturedBooksUseCase({required BaseBooklyRepo baseBooklyRepo})
+  FetchFeaturedBooksUseCase({required BaseHomeRepo baseBooklyRepo})
       : _baseBooklyRepo = baseBooklyRepo;
   @override
   Future<Either<Failure, List<BookEntity>>> call([int? param]) async {
