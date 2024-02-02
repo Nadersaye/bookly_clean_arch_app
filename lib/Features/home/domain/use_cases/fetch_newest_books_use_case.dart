dd9e24e9a904fcdaecfad48aa96ea081fbbbc40f
Nader <ns1568@fayoum.edu.eg>
2024-02-02 20:14:27 +0200

    Update import paths in fetch_newest_books_use_case.dart

--- lib/Features/home/domain/use_cases/fetch_newest_books_use_case.dart
+++ lib/Features/home/domain/use_cases/fetch_newest_books_use_case.dart
@@ -1,15 +1,15 @@
 import 'package:bookly_clean_arch_app/Features/home/domain/entities/book_entity.dart';
-import 'package:bookly_clean_arch_app/core/utils/errors/failure.dart';
-import 'package:bookly_clean_arch_app/core/utils/use_cases/base_use_case.dart';
-import 'package:bookly_clean_arch_app/core/utils/use_cases/no_param.dart';
+import 'package:bookly_clean_arch_app/core/errors/failure.dart';
+import 'package:bookly_clean_arch_app/core/use_cases/base_use_case.dart';
+import 'package:bookly_clean_arch_app/core/use_cases/no_param.dart';
 import 'package:dartz/dartz.dart';
 
-import '../repos/base_bookly_repo.dart';
+import '../repos/base_home_repo.dart';
 
 class FetchNewestBooksUseCase extends BaseUseCase<List<BookEntity>, NoParam> {
-  final BaseBooklyRepo _baseBooklyRepo;
+  final BaseHomeRepo _baseBooklyRepo;
 
-  FetchNewestBooksUseCase({required BaseBooklyRepo baseBooklyRepo})
+  FetchNewestBooksUseCase({required BaseHomeRepo baseBooklyRepo})
       : _baseBooklyRepo = baseBooklyRepo;
   @override
   Future<Either<Failure, List<BookEntity>>> call([NoParam? param]) async {
