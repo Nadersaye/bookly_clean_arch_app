d048a194939d6e0616f685000c9229106d3abd76
Nader <ns1568@fayoum.edu.eg>
2024-02-03 22:26:57 +0200

    Add FetchFeaturedBooksState to fetch featured books cubit

---
+++ lib/Features/home/presentation/manager/fetch featured books cubit/fetch_featured_books_state.dart
@@ -0,0 +1,30 @@
+part of 'fetch_featured_books_cubit.dart';
+
+sealed class FetchFeaturedBooksState extends Equatable {
+  const FetchFeaturedBooksState();
+
+  @override
+  List<Object> get props => [];
+}
+
+final class FetchFeaturedBooksInitial extends FetchFeaturedBooksState {}
+
+final class FetchFeaturedBooksLoading extends FetchFeaturedBooksState {}
+
+final class FetchFeaturedBooksLoaded extends FetchFeaturedBooksState {
+  final List<BookEntity> books;
+
+  const FetchFeaturedBooksLoaded({required this.books});
+
+  @override
+  List<Object> get props => [books];
+}
+
+final class FetchFeaturedBooksError extends FetchFeaturedBooksState {
+  final String errorMessage;
+
+  const FetchFeaturedBooksError({required this.errorMessage});
+
+  @override
+  List<Object> get props => [errorMessage];
+}
