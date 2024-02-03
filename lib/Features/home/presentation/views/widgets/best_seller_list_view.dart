26b7fd4595ceefd92c2f46065c07c7af24f6bda3
Nader <ns1568@fayoum.edu.eg>
2024-02-03 22:32:28 +0200

    Refactor BestSellerListView to accept a list of BookEntity

--- lib/Features/home/presentation/views/widgets/best_seller_list_view.dart
+++ lib/Features/home/presentation/views/widgets/best_seller_list_view.dart
@@ -1,20 +1,23 @@
+import 'package:bookly_clean_arch_app/Features/home/domain/entities/book_entity.dart';
 import 'package:flutter/material.dart';
 
 import 'best_seller_list_view_item.dart';
 
 class BestSellerListView extends StatelessWidget {
-  const BestSellerListView({super.key});
+  final List<BookEntity> books;
+  const BestSellerListView({super.key, required this.books});
 
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
       physics: const NeverScrollableScrollPhysics(),
       padding: EdgeInsets.zero,
-      itemCount: 10,
       itemBuilder: (context, index) {
-        return const Padding(
-          padding: EdgeInsets.symmetric(vertical: 10),
-          child: BookListViewItem(),
+        return Padding(
+          padding: const EdgeInsets.symmetric(vertical: 10),
+          child: BookListViewItem(
+            book: books[index],
+          ),
         );
       },
     );
