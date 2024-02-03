3312ee2b9d84464f52fb1c047c4ed5f03ee07569
Nader <ns1568@fayoum.edu.eg>
2024-02-03 22:30:21 +0200

    Add BestSellerListViewBlocBuilder widget

---
+++ lib/Features/home/presentation/views/widgets/best_seller_list_view_bloc_builder.dart
@@ -0,0 +1,31 @@
+import 'package:bookly_clean_arch_app/Features/home/presentation/views/widgets/best_seller_list_view.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import '../../manager/fetch newest books cubit/fetch_newest_books_cubit.dart';
+
+class BestSellerListViewBlocBuilder extends StatelessWidget {
+  const BestSellerListViewBlocBuilder({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<FetchNewestBooksCubit, FetchNewestBooksState>(
+      builder: (context, state) {
+        if (state is FetchNewestBooksLoading) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        } else if (state is FetchNewestBooksLoaded) {
+          return BestSellerListView(books: state.books);
+        } else if (state is FetchNewestBooksError) {
+          return Center(
+            child: Text(state.errorMessage),
+          );
+        } else {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+      },
+    );
+  }
+}
