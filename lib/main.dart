4f9c89fbee9426562aeab63d016fb3d45883961e
Nader <ns1568@fayoum.edu.eg>
2024-02-03 22:33:40 +0200

    Add book entity import and initialize Hive

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,22 @@
+import 'package:bookly_clean_arch_app/Features/home/domain/entities/book_entity.dart';
 import 'package:bookly_clean_arch_app/constants.dart';
+import 'package:bookly_clean_arch_app/core/utils/simple_bloc_observer.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'Features/home/presentation/manager/fetch featured books cubit/fetch_featured_books_cubit.dart';
+import 'Features/home/presentation/manager/fetch newest books cubit/fetch_newest_books_cubit.dart';
 import 'core/utils/app_router.dart';
+import 'core/utils/service_locator.dart';
 
-void main() {
+void main() async {
+  await Hive.initFlutter();
+  Hive.registerAdapter(BookEntityAdapter());
+  Bloc.observer = SimpleBlocObserver();
+  ServiceLocator.init();
+  await Hive.openBox<BookEntity>(kFeaturedBox);
+  await Hive.openBox<BookEntity>(kNewestBox);
   runApp(const Bookly());
 }
 
@@ -12,12 +25,25 @@ class Bookly extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routerConfig: AppRouter.router,
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark().copyWith(
-        scaffoldBackgroundColor: kPrimaryColor,
-        textTheme: GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<FetchFeaturedBooksCubit>(
+          create: (context) => FetchFeaturedBooksCubit(ServiceLocator.sl())
+            ..fetchFeaturedBooks(),
+        ),
+        BlocProvider<FetchNewestBooksCubit>(
+          create: (context) =>
+              FetchNewestBooksCubit(ServiceLocator.sl())..fetchNewestBooks(),
+        ),
+      ],
+      child: MaterialApp.router(
+        routerConfig: AppRouter.router,
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData.dark().copyWith(
+          scaffoldBackgroundColor: kPrimaryColor,
+          textTheme:
+              GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme),
+        ),
       ),
     );
   }
