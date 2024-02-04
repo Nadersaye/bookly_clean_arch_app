4d24cd6b9de51637ef190bdd5631b2120f0f35f7
Nader <ns1568@fayoum.edu.eg>
2024-02-04 17:19:10 +0200

    Refactor FetchNewestBooksUseCase to accept an integer parameter

--- lib/Features/home/domain/use_cases/fetch_newest_books_use_case.dart
+++ lib/Features/home/domain/use_cases/fetch_newest_books_use_case.dart
@@ -1,19 +1,17 @@
 import 'package:bookly_clean_arch_app/Features/home/domain/entities/book_entity.dart';
 import 'package:bookly_clean_arch_app/core/errors/failure.dart';
 import 'package:bookly_clean_arch_app/core/use_cases/base_use_case.dart';
-import 'package:bookly_clean_arch_app/core/use_cases/no_param.dart';
 import 'package:dartz/dartz.dart';
-
 import '../repos/base_home_repo.dart';
 
-class FetchNewestBooksUseCase extends BaseUseCase<List<BookEntity>, NoParam> {
+class FetchNewestBooksUseCase extends BaseUseCase<List<BookEntity>, int> {
   final BaseHomeRepo _baseHomeRepo;
 
   FetchNewestBooksUseCase({required BaseHomeRepo baseHomeRepo})
       : _baseHomeRepo = baseHomeRepo;
   @override
-  Future<Either<Failure, List<BookEntity>>> call([NoParam? param]) async {
+  Future<Either<Failure, List<BookEntity>>> call([int? param]) async {
     //manage permission here
-    return await _baseHomeRepo.fetchNewestBooks();
+    return await _baseHomeRepo.fetchNewestBooks(param!);
   }
 }
