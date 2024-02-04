0b4a13a8853a865706d3203d5da9c5c0f6d44c66
Nader <ns1568@fayoum.edu.eg>
2024-02-04 17:23:10 +0200

    Add SimilarBooksListViewBlocConsumer widget

--- lib/Features/home/presentation/views/widgets/similar_books_list_view_bloc_consumer.dart
+++ lib/Features/home/presentation/views/widgets/similar_books_list_view_bloc_consumer.dart
@@ -1,14 +1,24 @@
+import 'package:bookly_clean_arch_app/Features/home/domain/entities/book_entity.dart';
 import 'package:bookly_clean_arch_app/Features/home/presentation/views/widgets/similar_books_list_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import '../../manager/fetch newest books cubit/fetch_newest_books_cubit.dart';
 
-class SimilarBooksListViewBlocBuilder extends StatelessWidget {
-  const SimilarBooksListViewBlocBuilder({super.key});
+class SimilarBooksListViewBlocConsumer extends StatefulWidget {
+  const SimilarBooksListViewBlocConsumer({super.key});
 
   @override
+  State<SimilarBooksListViewBlocConsumer> createState() =>
+      _SimilarBooksListViewBlocConsumerState();
+}
+
+class _SimilarBooksListViewBlocConsumerState
+    extends State<SimilarBooksListViewBlocConsumer> {
+  List<BookEntity> books = [];
+  @override
   Widget build(BuildContext context) {
-    return BlocBuilder<FetchNewestBooksCubit, FetchNewestBooksState>(
+    return BlocConsumer<FetchNewestBooksCubit, FetchNewestBooksState>(
+      listener: (context, state) => {},
       builder: (context, state) {
         if (state is FetchNewestBooksLoading) {
           return const Center(
