9f587a6666721a116bfcda43c2ac9e1ecabf5151
Nader <ns1568@fayoum.edu.eg>
2024-02-04 17:18:19 +0200

    Refactor fetchFeaturedBooks and fetchNewestBooks to support pagination

--- lib/Features/home/data/data_sources/home_local_data_source.dart
+++ lib/Features/home/data/data_sources/home_local_data_source.dart
@@ -1,23 +1,35 @@
 import 'package:hive_flutter/hive_flutter.dart';
-
 import '../../../../constants.dart';
 import '../../domain/entities/book_entity.dart';
 
 abstract class BaseHomeLocalDataSource {
-  List<BookEntity> fetchFeaturedBooks();
-  List<BookEntity> fetchNewestBooks();
+  List<BookEntity> fetchFeaturedBooks({int pageNumber = 0});
+  List<BookEntity> fetchNewestBooks({int pageNumber = 0});
 }
 
 class HomeLocalDataSource implements BaseHomeLocalDataSource {
   @override
-  List<BookEntity> fetchFeaturedBooks() {
+  List<BookEntity> fetchFeaturedBooks({int pageNumber = 0}) {
+    int startIndex = pageNumber * 10;
+    int endIndex = (pageNumber + 1) * 10;
     var featuedBox = Hive.box<BookEntity>(kFeaturedBox);
-    return featuedBox.values.toList();
+    int length = featuedBox.length;
+    if (startIndex >= length || endIndex > length) {
+      return [];
+    }
+    return featuedBox.values.toList().sublist(startIndex, endIndex);
   }
 
   @override
-  List<BookEntity> fetchNewestBooks() {
-    var featuedBox = Hive.box<BookEntity>(kNewestBox);
-    return featuedBox.values.toList();
+  List<BookEntity> fetchNewestBooks({int pageNumber = 0}) {
+    int startIndex = pageNumber * 10;
+    int endIndex = (pageNumber + 1) * 10;
+
+    var newestBox = Hive.box<BookEntity>(kNewestBox);
+    int length = newestBox.length;
+    if (startIndex >= length || endIndex > length) {
+      return [];
+    }
+    return newestBox.values.toList().sublist(startIndex, endIndex);
   }
 }
